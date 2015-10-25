b84f2a3211f62f188aeb33a6850b8eef14bf447f
kaelvofraga <kaelfraga@hotmail.com>
2015-10-25 15:00:32 -0200

    [ADD] Avatar masculino completo. [DESC] Ajustes na responsvidade. [DESC] Tratamento de erros para o sistema do avatar. [DESC] Otimização no JSON do avatar.

--- www/js/controllers/AvatarCtrl.js
+++ www/js/controllers/AvatarCtrl.js
@@ -6,7 +6,10 @@
   angular.module('Pedal2Play')
     .controller('AvatarController', ['$scope', '$window', 'ImageService', 
                             function ($scope, $window, ImageService) 
-    {  
+    {      
+      $scope.supportsSVG = document.implementation.hasFeature(
+                            "http://www.w3.org/TR/SVG11/feature#Shape", "1.0");
+      
       ImageService.getAvatarPieces().then( function (avatarPieces) {
         var userGender = 'F';
         var Snap = $window.Snap;
@@ -17,20 +20,19 @@
           avatarSVG = avatarPieces.female;
         } else {
           avatarSVG = avatarPieces.male;
-        }
-
+        }            
+        
         Snap.load(avatarSVG, function (svg) {
           var baseGroup = svg.select(avatarPieces.base);
           drawingArea.append(baseGroup);
-          baseGroup.append(svg.select(avatarPieces.faces.cute));
-          baseGroup.append(svg.select(avatarPieces.shorts.professional));
-          baseGroup.append(svg.select(avatarPieces.jersey.professional));
-          baseGroup.append(svg.select(avatarPieces.gloves.professional));
-          baseGroup.append(svg.select(avatarPieces.shoes.professional));
-          baseGroup.append(svg.selectAll(avatarPieces.gradients));
-          baseGroup.append(svg.select(avatarPieces.glasses));
-          baseGroup.append(svg.select(avatarPieces.hairs.curly));
-          baseGroup.append(svg.select(avatarPieces.helmet));
+          baseGroup.append(svg.select(avatarPieces.faces.options[0].value));
+          baseGroup.append(svg.select(avatarPieces.shorts.options[0].value));
+          baseGroup.append(svg.select(avatarPieces.jersey.options[0].value));
+          baseGroup.append(svg.select(avatarPieces.gloves.options[0].value));
+          baseGroup.append(svg.select(avatarPieces.shoes.options[0].value));          
+          baseGroup.append(svg.select(avatarPieces.glasses.options[0].value));
+          baseGroup.append(svg.select(avatarPieces.hairs.options[0].value));
+          baseGroup.append(svg.select(avatarPieces.helmet.options[0].value));
         });            
       });      
     }]);
