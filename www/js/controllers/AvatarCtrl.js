fbc06dfa0280964ebbeb6d3737884aacdcf2e400
kaelvofraga <kaelfraga@hotmail.com>
2015-10-26 00:19:44 -0200

    [ADD] Ícones referentes as partes do avatar. [DESC] Modificada versão mínima do android. [DESC] Ajustada responsividade dos ícones e botões. [DESC] Quebrado grupo base do SVG do avatar em parte com roupa e sem. [DESC] Mudado template e usabilidade da tela do avatar. [DESC] Iniciado desenvolvimento da navegação entre as opções de customização.

--- www/js/controllers/AvatarCtrl.js
+++ www/js/controllers/AvatarCtrl.js
@@ -8,14 +8,52 @@
                             function ($scope, $window, ImageService) 
     {      
       $scope.supportsSVG = document.implementation.hasFeature(
-                            "http://www.w3.org/TR/SVG11/feature#Shape", "1.0");
+                            "http://www.w3.org/TR/SVG11/feature#Shape", "1.0");      
+                            
+      $scope.avatarPieces = null;
+      $scope.selectPiece = null;
+      $scope.options = null;
+      
+      var Snap = $window.Snap;
+      var drawingArea = Snap('.avatar');
+      var svgContent = null;
+      var currentOption = 0;
+      var avatarBaseGroup = {};    
+      
+      $scope.onNextOptionClick = function () {
+        if (currentOption < $scope.options.length) {
+          var value = $scope.options[currentOption].value;
+          var element = svgContent.select(value);
+          avatarBaseGroup.remove(element);
+          ++currentOption;
+          avatarBaseGroup.append(svgContent.select(
+              $scope.avatarPieces[$scope.selectPiece].options[currentOption].value));
+        }
+      }
+      
+      $scope.onPreviousOptionClick = function () {
+        
+      }
+               
+      $scope.onPieceClick = function (reference) {
+        $scope.selectPiece = reference;
+        if (!angular.isUndefined($scope.avatarPieces[reference])) {
+          $scope.options = $scope.avatarPieces[reference].options;
+        }
+      }
+      
+      $scope.getPath = function (reference) {
+        return "img/avatar/icons/" + reference + ".svg";
+      }
       
       ImageService.getAvatarPieces().then( function (avatarPieces) {
-        var userGender = 'F';
-        var Snap = $window.Snap;
-        var drawingArea = Snap('.avatar');
         var avatarSVG = '';
-
+        var userGender = 'F';
+        
+        $scope.avatarPieces = avatarPieces;
+        $scope.selectPiece = avatarPieces.icons[2].reference;
+        $scope.options = avatarPieces[$scope.selectPiece].options;
+         
         if (userGender === 'F') {
           avatarSVG = avatarPieces.female;
         } else {
@@ -23,17 +61,15 @@
         }            
         
         Snap.load(avatarSVG, function (svg) {
-          var baseGroup = svg.select(avatarPieces.base);
-          drawingArea.append(baseGroup);
-          baseGroup.append(svg.select(avatarPieces.faces.options[0].value));
-          baseGroup.append(svg.select(avatarPieces.shorts.options[0].value));
-          baseGroup.append(svg.select(avatarPieces.jersey.options[0].value));
-          baseGroup.append(svg.select(avatarPieces.gloves.options[0].value));
-          baseGroup.append(svg.select(avatarPieces.shoes.options[0].value));          
-          baseGroup.append(svg.select(avatarPieces.glasses.options[0].value));
-          baseGroup.append(svg.select(avatarPieces.hairs.options[0].value));
-          baseGroup.append(svg.select(avatarPieces.helmet.options[0].value));
-        });            
+          svgContent = svg;
+          avatarBaseGroup = svg.select(avatarPieces.base);
+          drawingArea.append(avatarBaseGroup);
+          avatarBaseGroup.append(svg.select(avatarPieces.faces.options[currentOption].value));
+          avatarBaseGroup.append(svg.select(avatarPieces.shorts.options[currentOption].value));
+          avatarBaseGroup.append(svg.select(avatarPieces.jerseys.options[currentOption].value));
+          avatarBaseGroup.append(svg.select(avatarPieces.shoes.options[currentOption].value));          
+          avatarBaseGroup.append(svg.select(avatarPieces.hairs.options[currentOption].value));
+        });  
       });      
     }]);
 })();
