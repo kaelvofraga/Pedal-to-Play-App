59932c8ea71e26656babf562b80ae4ecadc3b7d4
kaelvofraga <kaelfraga@hotmail.com>
2015-12-05 09:36:35 -0200

    [FIX] Ajuste na troca de idiomas. Bug ao trocar idioma pela primeira vez, quando não havia uma Setting salva no Localstorage.

--- www/js/services/SettingsService.js
+++ www/js/services/SettingsService.js
@@ -4,14 +4,16 @@
   angular.module('Pedal2Play')
     .factory('SettingsService', ['localStorageService', 
 				 	             function (localStorageService) { 
-									          		  
+		
+		  var defaultSetting = {language: 'pt-BR'};
+										          		  
 		  return {
 			getUserSettings: function () {
 				var user = localStorageService.get('user');
 				if (user !== null) {
-					return localStorageService.get('settings' + user.id);
+					return localStorageService.get('settings' + user.id) || defaultSetting;
 				}   
-				return {language: 'pt-BR'};
+				return defaultSetting;
 			},
 			setUserSettings: function (settings) {
 				var user = localStorageService.get('user');
